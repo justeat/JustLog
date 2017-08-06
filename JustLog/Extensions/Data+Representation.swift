832707466c16714fa53d7d3dcbdaee4bc3de4e95
Oron Ben Zvi <oronbz@gmail.com>
2017-08-06 17:39:18 +0300

    Fix downcast to String warning

--- JustLog/Extensions/Data+Representation.swift
+++ JustLog/Extensions/Data+Representation.swift
@@ -11,7 +11,7 @@ import Foundation
 extension Data {
     
     func stringRepresentation() -> String {
-        let retVal = NSString(data: self, encoding: String.Encoding.utf8.rawValue) as? String
+        let retVal = String(data: self, encoding: .utf8)
         return retVal ?? ""
     }
 }
