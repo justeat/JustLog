3cd64d56c73c60fbf875046e2ee103399a5c6f37
Alkiviadis Papadakis <alkiviadis.papadakis@just-eat.com>
2017-12-01 16:27:28 +0000

    CPD-10111: Added objc qualifier to exposed members.

--- JustLog/Extensions/Logger+ObjC.swift
+++ JustLog/Extensions/Logger+ObjC.swift
@@ -12,91 +12,91 @@ extension Logger {
     
     //MARK: verbose
     
-    public func verbose_objc(_ message: String) {
+    @objc public func verbose_objc(_ message: String) {
         self.verbose(message)
     }
     
-    public func verbose_objc(_ message: String, error: NSError?) {
+    @objc public func verbose_objc(_ message: String, error: NSError?) {
         self.verbose(message, error: error)
     }
     
-    public func verbose_objc(_ message: String, userInfo: [String : Any]?) {
+    @objc public func verbose_objc(_ message: String, userInfo: [String : Any]?) {
         self.verbose(message, userInfo: userInfo)
     }
     
-    public func verbose_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
+    @objc public func verbose_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
         self.verbose(message, error: error, userInfo: userInfo)
     }
     
     //MARK: debug
     
-    public func debug_objc(_ message: String) {
+    @objc public func debug_objc(_ message: String) {
         self.debug(message)
     }
     
-    public func debug_objc(_ message: String, error: NSError?) {
+    @objc public func debug_objc(_ message: String, error: NSError?) {
         self.debug(message, error: error)
     }
     
-    public func debug_objc(_ message: String, userInfo: [String : Any]?) {
+    @objc public func debug_objc(_ message: String, userInfo: [String : Any]?) {
         self.debug(message, userInfo: userInfo)
     }
     
-    public func debug_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
+    @objc public func debug_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
         self.debug(message, error: error, userInfo: userInfo)
     }
     
     //MARK: info
     
-    public func info_objc(_ message: String) {
+    @objc public func info_objc(_ message: String) {
         self.info(message)
     }
     
-    public func info_objc(_ message: String, error: NSError?) {
+    @objc public func info_objc(_ message: String, error: NSError?) {
         self.info(message, error: error)
     }
     
-    public func info_objc(_ message: String, userInfo: [String : Any]?) {
+    @objc public func info_objc(_ message: String, userInfo: [String : Any]?) {
         self.info(message, userInfo: userInfo)
     }
     
-    public func info_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
+    @objc public func info_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
         self.info(message, error: error, userInfo: userInfo)
     }
     
     //MARK: warning
     
-    public func warning_objc(_ message: String) {
+    @objc public func warning_objc(_ message: String) {
         self.warning(message)
     }
     
-    public func warning_objc(_ message: String, error: NSError?) {
+    @objc public func warning_objc(_ message: String, error: NSError?) {
         self.warning(message, error: error)
     }
     
-    public func warning_objc(_ message: String, userInfo: [String : Any]?) {
+    @objc public func warning_objc(_ message: String, userInfo: [String : Any]?) {
         self.warning(message, userInfo: userInfo)
     }
     
-    public func warning_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
+    @objc public func warning_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
         self.warning(message, error: error, userInfo: userInfo)
     }
     
     //MARK: error
     
-    public func error_objc(_ message: String) {
+    @objc public func error_objc(_ message: String) {
         self.error(message)
     }
     
-    public func error_objc(_ message: String, error: NSError?) {
+    @objc public func error_objc(_ message: String, error: NSError?) {
         self.error(message, error: error)
     }
     
-    public func error_objc(_ message: String, userInfo: [String : Any]?) {
+    @objc public func error_objc(_ message: String, userInfo: [String : Any]?) {
         self.error(message, userInfo: userInfo)
     }
     
-    public func error_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
+    @objc public func error_objc(_ message: String, error: NSError?, userInfo: [String : Any]?) {
         self.error(message, error: error, userInfo: userInfo)
     }
 }
