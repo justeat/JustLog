1ff5b47c09e8ee2fa56a952e7580e40997686c9a
Antonio Strijdom <antonio.strijdom@just-eat.com>
2020-06-26 14:55:12 +0100

    Tidy up and fixed queueing issues.

--- JustLog/Classes/LogstashDestination.swift
+++ JustLog/Classes/LogstashDestination.swift
@@ -21,7 +21,7 @@ private class LogstashDestinationURLSessionDelegate: NSObject, URLSessionDelegat
     
     func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         if logActivity {
-            print("🔌 <AsyncSocket>, did receive \(challenge.protectionSpace.authenticationMethod) challenge")
+            print("🔌 <LogstashDestination>, did receive \(challenge.protectionSpace.authenticationMethod) challenge")
         }
         if
             challenge.protectionSpace.host == self.host,
@@ -30,31 +30,46 @@ private class LogstashDestinationURLSessionDelegate: NSObject, URLSessionDelegat
             completionHandler(.useCredential, credential)
         } else {
             if logActivity {
-                print("🔌 <AsyncSocket>, Could not startTLS: invalid trust")
+                print("🔌 <LogstashDestination>, Could not startTLS: invalid trust")
             }
             completionHandler(.cancelAuthenticationChallenge, nil)
         }
     }
 }
 
+/// This entire class relies on `logDispatchQueue` to synchronise access to the `logsToShip` dictionary
+/// Every action is put in the `logDispatchQueue` which is synchronous, even though the actual sending
+/// via `URLSession` is not.
+/// Here's a brief summary of what operations are added to the queue and when:
+/// A log is generated via `send` and added to the `logsToShip` dictionary (along with a `tag` identifier).
+/// At some point `writeLogs` is called that creates an `NSURLSessionStreamTask` to send the log to the server.
+/// When the task completes, either `completeLog` or `retryLog` are called.
+///    `completeLog` removes the log from `logsToShip`
+///    `retryLog` clears the log from `logsInSocketQueue` and causes it to be resent the next time `writeLogs` is called.
+/// An optional `completionHandler` is called when all logs are written.
 public class LogstashDestination: BaseDestination  {
     
-    public var logzioToken: String?
-    
-    var logsToShip = [Int : [String : Any]]()
-    fileprivate var completionHandler: ((_ error: Error?) -> Void)?
-    private let logzioTokenKey = "token"
-    
-    var logActivity: Bool = false
-    let logDispatchQueue = OperationQueue()
-    let localSocketQueue = OperationQueue()
-    let session: URLSession
-    fileprivate let sessionDelegate: LogstashDestinationURLSessionDelegate
-    
+    /// Settings
     let allowUntrustedServer: Bool
     let host: String
     let port: Int
     let timeout: TimeInterval
+    var logActivity: Bool = false
+    public var logzioToken: String?
+    
+    /// Logs buffer
+    var logsToShip = [Int : [String : Any]]()
+    let logDispatchQueue = OperationQueue()
+    
+    /// Private
+    private var completionHandler: ((_ error: Error?) -> Void)?
+    private let logzioTokenKey = "token"
+    
+    /// Socket
+    private let localSocketQueue = OperationQueue()
+    private var session: URLSession
+    private let sessionDelegate: LogstashDestinationURLSessionDelegate
+    private var logsInSocketQueue = [Int]()
     
     @available(*, unavailable)
     override init() {
@@ -81,7 +96,12 @@ public class LogstashDestination: BaseDestination  {
     }
     
     public func cancelSending() {
+        self.logDispatchQueue.cancelAllOperations()
+        self.logsInSocketQueue = [Int]()
         self.session.invalidateAndCancel()
+        self.session = URLSession(configuration: .ephemeral,
+                                  delegate: sessionDelegate,
+                                  delegateQueue: localSocketQueue)
     }
     
     // MARK: - Log dispatching
@@ -101,6 +121,9 @@ public class LogstashDestination: BaseDestination  {
     }
 
     public func forceSend(_ completionHandler: @escaping (_ error: Error?) -> Void  = {_ in }) {
+
+        self.completionHandler = completionHandler
+
         writeLogs()
     }
     
@@ -110,38 +133,41 @@ public class LogstashDestination: BaseDestination  {
             
             guard let self = self else { return }
             
-            for log in self.logsToShip.sorted(by: { $0.0 < $1.0 }) {
+            let logsInQueue = self.logsInSocketQueue
+            let unprocessedLogs = self.logsToShip.filter({ (tag: Int, _) -> Bool in
+                !logsInQueue.contains(where: { $0 == tag })
+            })
+            guard unprocessedLogs.count > 0 else {
+                if self.logActivity {
+                    print("writeLogs() - nothing to write, \(logsInQueue.count) in URLSession queue")
+                }
+                return
+            }
+            let task = self.session.streamTask(withHostName: self.host, port: self.port)
+            if !self.allowUntrustedServer {
+                task.startSecureConnection()
+            }
+            for log in unprocessedLogs.sorted(by: { $0.0 < $1.0 }) {
                 let tag = log.0
                 let logData = self.dataToShip(log.1)
-                let task = self.session.streamTask(withHostName: self.host, port: self.port)
-                if !self.allowUntrustedServer {
-                    task.startSecureConnection()
-                }
                 task.write(logData, timeout: self.timeout) { (error) in
                     if let error = error {
                         if self.logActivity {
-                            print("🔌 <AsyncSocket>, did error: \(error.localizedDescription)")
+                            print("🔌 <LogstashDestination>, \(tag) did error: \(error.localizedDescription)")
                         }
-                        if let completionHandler = self.completionHandler {
-                            completionHandler(error)
-                        }
+                        self.retryLog(withTag: tag)
+                        self.callCompleteHandlerIfRequired(optionalError: error)
                     } else {
                         if self.logActivity {
-                            print("🔌 <AsyncSocket>, did write")
+                            print("🔌 <LogstashDestination>, did write \(tag)")
                         }
-                        self.logDispatchQueue.addOperation { [weak self, tag] in
-                            guard let self = self else { return }
-                            self.logsToShip[tag] = nil
-                        }
-                        if let completionHandler = self.completionHandler {
-                            completionHandler(nil)
-                        }
+                        self.completeLog(withTag:tag)
+                        self.callCompleteHandlerIfRequired()
                     }
-                    
-                    self.completionHandler = nil
                 }
-                task.resume()
+                self.logsInSocketQueue.append(tag)
             }
+            task.resume()
         }
     }
     
@@ -150,7 +176,6 @@ public class LogstashDestination: BaseDestination  {
             let time = mach_absolute_time()
             let logTag = Int(truncatingIfNeeded: time)
             self?.logsToShip[logTag] = dict
-            self?.writeLogs()
         }
     }
     
@@ -169,6 +194,61 @@ public class LogstashDestination: BaseDestination  {
         }
         
         return data
+    }
+    
+    func completeLog(withTag tag: Int) {
+        
+        self.logDispatchQueue.addOperation{ [weak self] in
+            guard let self = self else { return }
+            assert(self.logsInSocketQueue.count > 0, "Completed a session without any tasks scheduled!")
+            
+            // get the tag for this task & remove from queue
+            if self.logsInSocketQueue.contains(tag) {
+                self.logsInSocketQueue.removeAll(where: { $0 == tag })
+                self.logsToShip[tag] = nil
+            } else {
+                if self.logActivity {
+                    print("🔌 <LogstashDestination>, Completed task for tag (\(tag)")
+                }
+            }
+            
+            if self.logActivity {
+                print("🔌 <LogstashDestination>, \(self.logsInSocketQueue.count) remaining tasks")
+            }
+        }
+    }
+    
+    func retryLog(withTag tag: Int) {
+        
+        self.logDispatchQueue.addOperation{ [weak self] in
+            guard let self = self else { return }
+            
+            // get the tag for this task & remove from queue
+            if self.logsInSocketQueue.contains(tag) {
+                self.logsInSocketQueue.removeAll(where: { $0 == tag })
+            }
+            
+            if self.logActivity {
+                print("🔌 <LogstashDestination>, \(self.logsInSocketQueue.count) remaining tasks")
+            }
+        }
+    }
+    
+    func callCompleteHandlerIfRequired(optionalError error: Error? = nil) {
+        
+        self.logDispatchQueue.addOperation { [weak self] in
+            guard let self = self else { return }
+            
+            if self.logsInSocketQueue.count <= 0 {
+                if let completionHandler = self.completionHandler {
+                    if self.logActivity {
+                        print("🔌 <LogstashDestination>, calling completion handler")
+                    }
+                    completionHandler(error)
+                }
+                self.completionHandler = nil
+            }
+        }
     }
     
 }
